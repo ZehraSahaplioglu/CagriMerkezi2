24b732a324a4aad7cf2c332362e9864813a5bd75
55279283562 <sahapliogluzehra@gmail.com>
2023-12-07 15:43:29 +0300

    footer ayarlaması yapıldı

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,11 +32,6 @@ namespace CagriMerkezi2.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
