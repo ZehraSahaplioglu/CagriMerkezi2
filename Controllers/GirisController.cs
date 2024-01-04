7d3d9d73290d12d344d61cb04e416c0b8d8e0fb0
55279283562 <sahapliogluzehra@gmail.com>
2024-01-04 15:02:39 +0300

    Yeni düzenkemeler yapıldı

---
+++ Controllers/GirisController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CagriMerkezi2.Controllers
+{
+    public class GirisController : Controller
+    {
+
+        private readonly ILogger<GirisController> _logger;
+
+        public GirisController(ILogger<GirisController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
